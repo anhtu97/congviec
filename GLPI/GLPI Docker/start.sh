c6d93e2d479d09dbf0f1f285cdf3ad6461ca8b37
anhtu97 <48386226+anhtu97@users.noreply.github.com>
2020-08-05 09:50:39 +0700

    Update start.sh

--- GLPI/GLPI Docker/start.sh
+++ GLPI/GLPI Docker/start.sh
@@ -1,39 +1,21 @@
-yum -y install wget curl nano git 
+chmod -R 755 /var/www/html/glpi
 
-cd /var/www/
-
-wget https://github.com/glpi-project/glpi/releases/download/9.3.1/glpi-9.3.1.tgz
-
-tar -xvzf glpi-9.3.1.tgz
-
-mv glpi glpi-9.3.1
-
-ln -s glpi-9.3.1 glpi
-
-chmod -R 755 /var/www/glpi-9.3.1
-
-chown -R apache:apache /var/www/glpi-9.3.1
+chown -R apache:apache /var/www/html/glpi
 
 echo '<VirtualHost *:80>
-
         ServerAdmin localhost
         ServerName localhost
-        DocumentRoot /var/www/glpi
-
+        DocumentRoot /var/www/html/glpi
         ErrorLog "/var/log/httpd/glpi.com.log"
         CustomLog "/var/log/httpd/glpi.com.log" combined
-
-        <Directory> /var/www/glpi/config>
+        <Directory> /var/www/html/glpi/config>
                 AllowOverride None
                 Require all denied
         </Directory>
-
-        <Directory> /var/www/glpi/files>
+        <Directory> /var/www/html/glpi/files>
                 AllowOverride None
                 Require all denied
         </Directory>
-
 </VirtualHost>' > /etc/httpd/conf.d/glpi.conf
 
 rm -rf /run/httpd/* /tmp/httpd*
-exec /usr/sbin/apachectl -DFOREGROUND
